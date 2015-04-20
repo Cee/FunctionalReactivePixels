35862912073afe146e2f8ad5ad3637c1084e2f97
Cee <cee@chu2byo.com>
2015-04-20 08:32:05 +0800

    Initial Commit

---
+++ FunctionalReactivePixels/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FunctionalReactivePixels
+//
+//  Created by Cee on 20/04/2015.
+//  Copyright (c) 2015 Cee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FunctionalReactivePixels/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FunctionalReactivePixels
+//
+//  Created by Cee on 20/04/2015.
+//  Copyright (c) 2015 Cee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
